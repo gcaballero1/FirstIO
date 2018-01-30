23f3de421be4e3615733b6340e80280529af030a
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-01-30 11:52:53 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author: Gabriel Caballero
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Gabriel Caballero. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
